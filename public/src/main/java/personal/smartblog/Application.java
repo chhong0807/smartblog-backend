7df5938e2f985b008b941560a3db6c6a445a6a1a
chhong0807 <32008621+chhong0807@users.noreply.github.com>
2017-09-28 13:09:53 +0900

    Add files via upload
    Spring Boot Web 초기 설정
     - common : 공통 설정 및 컴포넌트 
     - admin : 관리 페이지 관련 컴포넌트
     - public : 사용자 페이지 관련 컴포넌트

---
+++ public/src/main/java/personal/smartblog/Application.java
@@ -0,0 +1,15 @@
+package personal.smartblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by skplanet on 2017-09-28.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
